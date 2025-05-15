d66ea4b7244b890a2b9c1b268caa46664e659daf
Igor Novitskii <drmnnml@gmail.com>
2025-05-15 23:44:03 +0300

    codewars-josephus-addf

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+//
+//  functions.cpp
+//  algorithms
+//
+//  Created by Igor on 15.05.2025.
+//
+
+#include "functions.hpp"
+
+auto add_f(int n)
+{
+   return Yoba(n);
+}
---
+++ vectors.hpp
@@ -0,0 +1,22 @@
+//
+//  vectors.hpp
+//  algorithms
+//
+//  Created by Igor on 15.05.2025.
+//
+
+#ifndef vectors_hpp
+#define vectors_hpp
+
+#include <stdio.h>
+#include <vector>
+
+#endif /* vectors_hpp */
+
+
+/*
+ You are now to create a function that returns a Josephus permutation,
+ taking as parameters the initial array/list of items to be permuted
+ as if they were in a circle and counted out every k places until none remained.
+*/
+std::vector < int> josephus(std::vector < int > items, int k);
