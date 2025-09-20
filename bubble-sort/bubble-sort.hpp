d8ea5d0d3ce37957c9063d64e0dd89dcb5ce3af1
Igor Novitskii <drmnnml@gmail.com>
2025-09-20 14:17:08 +0300

    bubble sort basic implementation

---
+++ bubble-sort/bubble-sort.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void bubbleSort(std::vector<T>& arr) 
+{  
+  int n = arr.size();
+  
+  for (int i = 0; i < n - 1; ++i) 
+  {
+    for (int j = 0; j < n - i - 1; ++j) 
+    {
+      if (arr[j] > arr[j + 1]) 
+      {
+        std::swap(arr[j], arr[j + 1]);     
+      }
+    }
+  }
+}
