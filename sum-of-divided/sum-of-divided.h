fd180d76f23625f24dcf3166a40214f83d3f0595
Igor Novitskii <drmnnml@gmail.com>
2025-09-24 07:35:42 +0300

    feat: codewars-sum-of-divided

--- sum-of-divided/sum-of-divided.h
+++ sum-of-divided/sum-of-divided.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <sstream>
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <map>
+#include <string>
 
 bool isBase(int num) {
   return num == 2 || num == 3 || num == 5;
@@ -27,18 +30,32 @@ bool isPrime(int num)
   return true;
 }
 
-std::string sumOfDivided(std::vector<int> &lst) {
+std::string sumOfDivided(std::vector<int> &lst) 
+{
   int max = *std::max_element(lst.begin(), lst.end());
+  int min = *std::min_element(lst.begin(), lst.end());
+  int abs_max = std::max(std::abs(max), std::abs(min));
+  std::map<int, int> divided_map{};
   //For each number find all prime dividers
-  for (int i = 2; i <= max; i++) {
+  for (int i = 2; i <= abs_max; i++) {
     if (isPrime(i)) {
       for (int j = 0; j < lst.size(); j++) {
         if (lst[j] % i == 0) {
           // Store dividers in table where column is prime number, dividers are the values
-        } 
+          auto current_sum = divided_map.find(i);
+          if (current_sum != divided_map.end()) {
+            current_sum->second += lst[j];
+          } else { // is not found
+            divided_map.insert({i,lst[j]});
+          };
+        }
       }
     }
   }
   // Build final string
-  return "";
+  std::stringstream out;
+  for (auto const& [key, val] : divided_map) {
+    out << "(" << key << " " << val << ")";
+  } 
+  return out.str();
 }
