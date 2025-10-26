0e44bfd73aa7c54fc72a40c7ebbacf15a912ef1f
Igor Novitskii <drmnnml@gmail.com>
2025-10-26 10:58:33 +0300

    feat: number partitions additional solutions

--- number-partitions/partitions.h
+++ number-partitions/partitions.h
@@ -42,3 +42,56 @@ uint32_t find_paritions_basic(uint32_t n)
   return vector[n];
 }
 
+/**
+ * Method using Euler's pentagonals 
+ */
+
+std::vector<uint32_t> pentagonals;
+
+uint32_t pentagonal(uint32_t n) {
+  if (n >= pentagonals.size()) {
+    for (uint32_t i = pentagonals.size(); i <= n; ++i) {
+      int32_t k = (i + 1) / 2;
+      if (i % 2 == 0) {
+        k = -k;
+      }
+      pentagonals.push_back((3 * k * k - k) / 2);
+    }
+  }
+  return pentagonals[n];
+}
+
+std::vector<uint32_t> ps{1, 1};
+
+uint32_t find_partitions_pentagonal(uint32_t n) {
+  if (n >= ps.size()) ps.resize(n + 1, 0);
+  if (ps[n] == 0) {
+    int32_t res = 0;
+    for (uint32_t i = 1; i <= n; ++i) {
+      auto pent = pentagonal(i);
+      auto prev = ((int32_t) n) - ((int32_t) pent);
+      if (prev < 0) break;
+      int32_t p = find_partitions_pentagonal(prev);
+      if (i % 4 == 0 || i % 4 == 3) {
+        p = -p;
+      }
+      res += p;
+    }
+    ps[n] = res;
+  }
+  return ps[n];
+}
+
+/**
+ * TODO: description 
+ */
+
+uint32_t find_partitions_reverse(uint32_t n, uint32_t k) {
+  if(n==1 || k==1)  return 1;
+  if(n<=k)  return find_partitions_reverse(n,n-1)+1;
+  return find_partitions_reverse(n,k-1)+find_partitions_reverse(n-k,k);
+}
+
+uint32_t find_partitions_reverse(uint32_t n) {
+    return find_partitions_reverse(n, n);
+}
