4e8e136fb4556b7e4283519fa86cb47f88c2aec3
Igor Novitskii <drmnnml@gmail.com>
2025-10-26 10:57:26 +0300

    feat: add numeric.h. nultiples35, square digits

---
+++ math/numeric.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <string>
+
+/**
+ * https://projecteuler.net/problem=1
+ * all numbers from 1 to 15 / 2 = all numbers that divides by 3 and 5
+ * this idea not working yet, dont know why
+ **/
+int multiples35(int number)
+{
+    int remain = (number - 1) % 15;
+    int base = (number - 1) - remain;
+    int result = (1 + base) * base / 4;
+    for (int i = remain; i >= 3; --i) {
+        if (i % 5 == 0 || i % 3 == 0)
+            result += base + i;
+    }
+    return result;
+}
+
+int multiples35_slow(int number)
+{
+    int result = 0;
+    for (int i = number - 1; i >= 3; --i) {
+        if (i % 5 == 0 || i % 3 == 0)
+            result += i;
+    }
+    return result;
+}
+
+/**
+ https://en.wikipedia.org/wiki/Gauss_sum
+ **/
+int multiples35_gauss(int number)
+{
+  int n = number - 1;
+  return (3 * (n / 3) * ((n / 3) + 1) / 2) + (5 * (n / 5) * ((n / 5) + 1) / 2) - (15 * (n / 15) * ((n / 15) + 1) / 2);
+}
+
+
+/**
+ * TODO: description
+ */
+int multiples35_gauss_2(int number)
+{
+  int threes, fives, fifteens;
+  int sum_threes, sum_fives, sum_fifteens;
+  threes = (number - 1) / 3;
+  fives = (number - 1) / 5;
+  fifteens = (number - 1)/ 15;
+  sum_threes = 3 * (threes * (threes + 1)) / 2;
+  sum_fives = 5 * (fives * (fives + 1)) / 2;
+  sum_fifteens = 15 * (fifteens * (fifteens + 1)) / 2;
+  return sum_threes + sum_fives - sum_fifteens;
+}
+
+
+
+/**
+ * Square digits
+ * TODO: description
+ */
+
+ 
+int square_digits_best(int num) {
+    int result = 0;
+    int mult = 1;
+    while (num > 0) {
+        int d = num % 10;
+        result += mult * d * d;
+        mult *= d > 3 ? 100 : 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int square_digits_smart(int num) {
+    std::string s = std::to_string(num);
+    std::string ans;
+    for(char c: s){
+        int i = c - '0';
+        ans += std::to_string(i * i);
+    }
+    return std::stoi(ans);
+}
